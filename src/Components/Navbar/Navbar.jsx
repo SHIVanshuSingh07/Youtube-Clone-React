7901316cae4c86aa8b88051b9a2e62f929d18ad5
Shivanshu singh <sivanshusingh2004@gmail.com>
2024-03-29 13:36:01 +0530

    navbar jsx change

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,6 +3,10 @@ import "./Navbar.css";
 import menu_icon from "../../assets/menu.png";
 import logo from "../../assets/logo.png";
 import search_icon from "../../assets/search.png";
+import upload_icon from "../../assets/upload.png";
+import more_icon from "../../assets/more.png";
+import notification_icon from "../../assets/notification.png";
+import profile_icon from "../../assets/jack.png";
 const Navbar = () => {
   return (
     <nav className="flex-div">
@@ -15,10 +19,10 @@ const Navbar = () => {
         <img src={search_icon} alt="" />
       </div>
       <div className="nav-right flex-div">
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
+        <img src={upload_icon} alt="" />
+        <img src={more_icon} alt="" />
+        <img src={notification_icon} alt="" />
+        <img src={profile_icon} alt="" className='user-icon' />
       </div>
     </nav>
   );
