175ad9870bd87f114e46c5520e46cb40ffdd6d9b
Shivanshu singh <sivanshusingh2004@gmail.com>
2024-04-02 00:05:27 +0530

    for side seter

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Home.css'
 import Sidebar from '../../Components/Sidebar/Sidebar'
-const Home = (sidebar) => {
+const Home = ({sidebar}) => {
   return (
     <>
     <Sidebar sidebar={sidebar}/>
